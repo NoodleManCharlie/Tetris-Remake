e98ad544bb201db1b4f857f194e2bb420613f937
Charlie Carter <cmcarter1908@gmail.com>
2024-05-28 12:34:41 -0400

    Quick note

--- Win/Tetris.cpp
+++ Win/Tetris.cpp
@@ -606,4 +606,4 @@ It basically provides a set of APIs to interact with various devices like graphi
 Look into this and how it deals with graphics. Also how widley it is used
 */
 
-//Checkout https://www.youtube.com/watch?v=dc7CIkZcWYE 
+//Checkout https://www.youtube.com/watch?v=dc7CIkZcWYE - Atlas OS makes Windows Faster
